fedad1c0ac2cafbc01954a568c439c4fc15c6b62
eryihaha <2550541112@qq.com>
2022-08-20 20:36:27 +0800

    fix bug

--- utils/cat_train_data.py
+++ utils/cat_train_data.py
@@ -1,8 +1,8 @@
 import os
 root_data_path = '../../reco_search_data/'
 data_path = ["train_data_reco_1.txt", "train_data_reco_2.txt", "train_data_reco_3.txt", 'train_data_reco_4.txt', 'train_final_data_src.txt']
+all_train_data = []
 for file_name in data_path:
-    all_train_data = []
     with open(os.path.join(root_data_path, file_name), 'r') as f:
         for line in f:
             all_train_data.append(line)
